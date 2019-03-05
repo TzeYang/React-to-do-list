96eae93318ae590531b6e96e7ebc32114d705ac3
Tze Yang Chew <tzeyangchew@MacBook-Pro.local>
2019-03-05 22:35:56 +0800

    Updates

---
+++ precache-manifest.1ba152ea3d1a7be231a19a0ce7a3ae3d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "021acb0de74640db1bcc",
+    "url": "/React-to-do-list/static/js/runtime~main.021acb0d.js"
+  },
+  {
+    "revision": "85e398c12cd1963e001b",
+    "url": "/React-to-do-list/static/js/main.85e398c1.chunk.js"
+  },
+  {
+    "revision": "2dbbcde96f5dc3f454f6",
+    "url": "/React-to-do-list/static/js/2.2dbbcde9.chunk.js"
+  },
+  {
+    "revision": "85e398c12cd1963e001b",
+    "url": "/React-to-do-list/static/css/main.26b7abff.chunk.css"
+  },
+  {
+    "revision": "070b1bc6c0867e484f15d10977da8d63",
+    "url": "/React-to-do-list/index.html"
+  }
+];
