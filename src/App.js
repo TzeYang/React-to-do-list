e3582288e4a325a4ab330c68ed2d71c81f281339
Tze Yang Chew <tzeyangchew@MacBook-Pro.local>
2019-03-05 22:30:02 +0800

    todolist

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './App.css';
+import ToDoCreate from './components/ToDoCreate';
+
+class App extends Component {
+  render() {
+    return (
+      <ToDoCreate/>
+    );
+  }
+}
+export default App;
